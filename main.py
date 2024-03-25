60651c1c54bc0639e4398cf2c314989ab31bf7f2
DabGias <gabrielfurlaneti@gmail.com>
2024-03-25 18:08:05 -0300

    new: vercel config

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from threading import Thread
 
 from bot import bot
-from webserver import keep_alive
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "bot online"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    
+    t.start()
+    
 
 if __name__ == '__main__': 
     keep_alive()
