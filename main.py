9221bdd99ea23e1fc0ef7e9e21ab6aea708f0055
DabGias <gabrielfurlaneti@gmail.com>
2024-03-25 18:53:44 -0300

    edit: main.py

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from threading import Thread
+from http.server import BaseHTTPRequestHandler
 
 from bot import bot
 
-app = Flask(__name__)
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write("bot online!".encode('utf-8'))
 
-@app.route("/")
-def home():
-    return "bot online"
+        app = Flask(__name__)
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
+        def run():
+            app.run(host="0.0.0.0", port=8080)
 
-def keep_alive():
-    t = Thread(target=run)
-    
-    t.start()
-    
+        def keep_alive():
+            t = Thread(target=run)
+            
+            t.start()
+            
 
-if __name__ == '__main__': 
-    keep_alive()
+        if __name__ == '__main__': 
+            keep_alive()
 
-    load_dotenv()
+            load_dotenv()
 
-    bot.run(os.getenv("DISCORD_TOKEN_SECRET"))
+            bot.run(os.getenv("DISCORD_TOKEN_SECRET"))
+
