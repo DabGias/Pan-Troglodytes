5b8ae771dec88245dcbaf5094557463e94fc07f5
DabGias <gabrielfurlaneti@gmail.com>
2024-03-25 17:39:04 -0300

    new: Flask webserver

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 from dotenv import load_dotenv
-from bot import bot
 
-if __name__ == '__main__':
+from bot import bot
+from webserver import keep_alive
+
+if __name__ == '__main__': 
+    keep_alive()
+
     load_dotenv()
 
     bot.run(os.getenv("DISCORD_TOKEN_SECRET"))
